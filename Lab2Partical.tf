b3113ec0bbf31e335bed60574072407af08a3b39
“Ronan.Doherty” <L00137900@atu.ie>
2024-11-19 21:03:06 +0000

    add aws sample file

---
+++ Lab2Partical.tf
@@ -0,0 +1,37 @@
+from constructs import Construct
+from cdktf import App, TerraformStack
+from imports.aws.instance import Instance
+from imports.aws.provider import AwsProvider
+
+class MyMultipleStacksConfig:
+    environment: str
+    region: str = None
+    def __init__(self, environment: str, region: str = None):
+        self.environment = environment
+        self.region = region
+
+
+class MyMultipleStacks(TerraformStack):
+    def __init__(self, scope: Construct, id: str, config: MyMultipleStacksConfig):
+        super().__init__(scope, id)
+
+        region = "us-east-1" if config.region == None else config.region
+
+        AwsProvider(self, "aws",
+            region = region
+        )
+
+        Instance(self, "Hello",
+            ami = "ami-2757f631",
+            instance_type = "t2.micro",
+            tags = {
+                "environment": config.environment,
+            }
+        )
+
+multi_stack_app = App()
+MyMultipleStacks(multi_stack_app, "multiple-stacks-dev", MyMultipleStacksConfig(environment = "dev"))
+MyMultipleStacks(multi_stack_app, "multiple-stacks-staging", MyMultipleStacksConfig(environment = "staging"))
+MyMultipleStacks(multi_stack_app, "multiple-stacks-production-us", MyMultipleStacksConfig(environment = "staging", region = "eu-central-1"))
+
+multi_stack_app.synth
