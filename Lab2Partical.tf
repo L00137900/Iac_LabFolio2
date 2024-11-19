662de64ca13237466769edd66735bd3bfd3e9068
“Ronan.Doherty” <L00137900@atu.ie>
2024-11-19 22:24:44 +0000

    Add comments to tf file to aid documentation

--- Lab2Partical.tf
+++ Lab2Partical.tf
@@ -3,6 +3,7 @@ from cdktf import App, TerraformStack
 from imports.aws.instance import Instance
 from imports.aws.provider import AwsProvider
 
+#Configuration for stack settings
 class MyMultipleStacksConfig:
     environment: str
     region: str = None
@@ -10,17 +11,19 @@ class MyMultipleStacksConfig:
         self.environment = environment
         self.region = region
 
-
+##Initalise terraform stack using the above configuration
 class MyMultipleStacks(TerraformStack):
     def __init__(self, scope: Construct, id: str, config: MyMultipleStacksConfig):
         super().__init__(scope, id)
 
+#Selecting the region in AWS
         region = "eu-west-1" if config.region == None else config.region
 
         AwsProvider(self, "aws",
             region = region
         )
 
+#Creating an EC2 instance
         Instance(self, "Hello",
             ami = "ami-0fcc0bef51bad3cb2",
             instance_type = "t2.micro",
@@ -29,6 +32,7 @@ class MyMultipleStacks(TerraformStack):
             }
         )
 
+#Setup for stack
 multi_stack_app = App()
 MyMultipleStacks(multi_stack_app, "multiple-stacks-dev", MyMultipleStacksConfig(environment = "dev"))
 MyMultipleStacks(multi_stack_app, "multiple-stacks-staging", MyMultipleStacksConfig(environment = "staging"))
