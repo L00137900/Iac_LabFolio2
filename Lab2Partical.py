cc89814256372feaf10e945c505b3118f97671d0
“Ronan.Doherty” <L00137900@atu.ie>
2024-11-25 14:50:40 +0000

    Add more tags to EC2 instance and alter some comments to imrpove governance

--- Lab2Partical.py
+++ Lab2Partical.py
@@ -1,8 +1,9 @@
 from constructs import Construct
 from cdktf import App, TerraformStack
-from imports.aws.instance import Instance
+from imports.aws.ec2 import Instance
 from imports.aws.provider import AwsProvider
 
+#Default configurations
 DEFAULT_AMI_ID = "ami-0fcc0bef51bad3cb2"   # AMI ID for EC2 instance
 DEFAULT_INSTANCE_TYPE = "t2.micro"         # EC2 instance type
 DEFAULT_REGION = "eu-west-1"               # Default Region
@@ -11,17 +12,18 @@ DEFAULT_REGION = "eu-west-1"               # Default Region
 class MyMultipleStacksConfig:
     environment: str
     region: str = None
+
     def __init__(self, environment: str, region: str = None):
         self.environment = environment
-        self.region = region
+        self.region = region or DEFAULT_REGION
 
 ##Initalise terraform stack using the above configuration
 class MyMultipleStacks(TerraformStack):
     def __init__(self, scope: Construct, id: str, config: MyMultipleStacksConfig):
         super().__init__(scope, id)
 
-#Selecting the region in AWS
-        region = config.region or DEFAULT_REGION
+#Specifying the AWS Provider region
+        region = DEFAULT_REGION or config.region
 
         AwsProvider(self, "aws",
             region = region
@@ -32,7 +34,10 @@ class MyMultipleStacks(TerraformStack):
             ami = DEFAULT_AMI_ID,
             instance_type = DEFAULT_INSTANCE_TYPE,
             tags = {
-                "environment": config.environment,
+                "environment": config.environment,      #Specifies environment across deployment stages
+                "region": region,                       #Specify region
+                "owner": "FridayHITT",                  #Specifying the team managing the EC2 instance
+                "use case": "Web site server"              #Specifying the use case of the EC2 instance
             }
         )
 
@@ -40,6 +45,6 @@ class MyMultipleStacks(TerraformStack):
 multi_stack_app = App()
 MyMultipleStacks(multi_stack_app, "multiple-stacks-dev", MyMultipleStacksConfig(environment = "dev"))
 MyMultipleStacks(multi_stack_app, "multiple-stacks-staging", MyMultipleStacksConfig(environment = "staging"))
-MyMultipleStacks(multi_stack_app, "multiple-stacks-production-us", MyMultipleStacksConfig(environment = "staging", region = "eu-central-1"))
+MyMultipleStacks(multi_stack_app, "multiple-stacks-production-us", MyMultipleStacksConfig(environment = "staging", region = "eu-west-1"))
 
 multi_stack_app.synth
